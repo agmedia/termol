9d1bb47b3e9505779299fa3a598595e1e6727f2f
tomashina <tomislav@agmedia.hr>
2026-02-27 15:19:42 +0100

    fix Fit Finder

--- resources/views/front/partials/splide-assets.blade.php
+++ resources/views/front/partials/splide-assets.blade.php
@@ -4,9 +4,9 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css">
         <style>
             /* Prevent layout jump before Splide CSS is downloaded. */
-            .splide__track { overflow: hidden; }
-            .splide__list { display: flex; margin: 0; padding: 0; list-style: none; }
-            .splide__slide { flex: 0 0 100%; min-width: 0; }
+            .splide:not(.is-initialized) .splide__track { overflow: hidden; }
+            .splide:not(.is-initialized) .splide__list { display: flex; margin: 0; padding: 0; list-style: none; }
+            .splide:not(.is-initialized) .splide__slide { flex: 0 0 100%; min-width: 0; }
         </style>
     @endpush
 
